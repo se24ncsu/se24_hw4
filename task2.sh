b764db3f7df4b41d228efa4fad2e150c38d11c0b
Srimadh V Rao <64469917+Shrimadh@users.noreply.github.com>
2024-10-10 21:09:27 -0400

    Task2.sh

---
+++ task2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+grep -rl "sample" ./dataset1 | xargs grep -o "CSC510" | cut -d: -f1| xargs ls -l | uniq -c | grep -E "[3-9] -|[0-9]{2,} -" | gawk '{print $1 ,$6, $10}'| sort -k1,1nr -k2,2nr| sed 's/file/filtered/'| gawk '{print $3}'
